9c48b6107e50eb158153dbca70f975e3733a9676
charles-ram <ram.charles429@gmail.com>
2023-03-11 23:58:47 -0600

    added prompt and functions

--- server.js
+++ server.js
@@ -37,5 +37,43 @@ const mainPrompt = [
     }
 ];
 
+// Prompts user with inquirer and executes function
 function cli_prompt() {
+    // Prompt user actions using inquirer
+    inquirer.prompt(mainPrompt)
+
+    // Await user response
+    .then(function(answer) {
+
+        // Execute function viewDept if user selection is "View departments"
+        if(answer.action == "View employees") {
+            viewAll();
+        } else if (answer.action == "View departments") {
+            viewDept();
+        } else if (answer.action == "View roles") {
+            viewRoles();
+        } else if (answer.action == "Add employee") {
+            addEmployee();
+        } else if (answer.action == "Add department") {
+            addDept();
+        } else if (answer.action == "Add role") {
+            addRole();
+        } else if (answer.action == "Edit employee") {
+            updateEmployee();
+        } else if (answer.action == "Remove employee") {
+            deleteEmployee();
+        } else if (answer.action == "EXIT") {
+            exit();
+        };
+    });
+};
+
+// Views all employees in employee_db
+function viewAll() {
+    let query =
+        "SELECT employees.first_name, employee.last_name, roles.title, roles.salary, department.dept_name AS department, employee.manager_id " + 
+        "FROM employees " + 
+        "JOIN roles ON roles.id = employees.role_id " + 
+        "JOIN department ON roles.derpoartment_id = department.id " + 
+        "ORDER BY employees.id;"
 }
